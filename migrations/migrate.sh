dfcdfdeaa039ff70c12b7d383ceb41528f9c08dc
desafinadude <meerkdir@gmail.com>
2025-11-17 18:17:18 +0200

    add migrations

--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -10,9 +10,16 @@ FILES=(
     "migrations/06-11-25-add-schema-and-version-to-pathogens.sql"
     "migrations/13-11-2025-submission-workflow-updates.sql"
     "migrations/14-11-2025-isolates-table.sql"
+    "migrations/14-11-2025-2-add-isolates-sequence-error-column.sql"
+    "migrations/14-11-2025-3-submissions-table-work.sql"
+    "migrations/15-11-2025-add-back-isolate-id-column.sql"
+    "migrations/16-11-2025-schemas-table.sql"
+    "migrations/16-11-2025-2-drop-submission-log-table.sql"
+    "migrations/16-11-2025-3-remove-schema-name-column-from-pathogen-table.sql"
 )
 
 for file in "${FILES[@]}"; do
     echo "Running $file..."
     psql -U "$DB_USER" -h "$DB_HOST" -p "$DB_PORT" -d "$DB_NAME" < "$file"
 done
+
